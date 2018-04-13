8f2e4f14f84bf0935c4c720b4e372155c5c1b095
a649725514 <649725514@qq.com>
2018-04-13 13:53:56 +0800

    日历

--- src/pages/piechart.js
+++ src/pages/piechart.js
@@ -11,6 +11,7 @@ import {
     ArcSeries,
     RadialChart
 } from 'react-vis';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { curveCatmullRom } from 'd3-shape';
 const PI = Math.PI;
 export default class Piechart extends Component {
@@ -33,77 +34,81 @@ export default class Piechart extends Component {
                 alignItems: 'center'
             }}>
                 <Topbar title="工作统计" />
-                <div></div>
-                <div style={{
-                    display: 'flex',
-                    width: this.state.width * 0.85,
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
+                <Scrollbars style={{ width: this.state.width * 0.85, height: this.state.height * 0.94 }}>
+                    <div>
+                        <img style={{ width: this.state.width * 0.85 }} src={require('../pic/rili.png')}></img>
+                    </div>
                     <div style={{
                         display: 'flex',
-                        width: this.state.width * 0.85 / 2,
-                        flexDirection: 'column',
+                        width: this.state.width * 0.85,
+                        flexDirection: 'row',
                         justifyContent: 'center',
                         alignItems: 'center'
                     }}>
-                        <XYPlot
-                            xType="ordinal"
-                            width={300}
-                            height={300}
-                            xDistance={100}
-                        >
-                            <VerticalGridLines />
-                            <HorizontalGridLines />
-                            <XAxis style={{
-                                line: { stroke: 'grey' },
-                            }} title="日期" position='start' />
-                            <YAxis style={{
-                                line: { stroke: 'grey' },
-                            }} title="工作量" />
-                            <BarSeries
-                                color='rgb(92,172,238)'
-                                className="vertical-bar-series-example"
+                        <div style={{
+                            display: 'flex',
+                            width: this.state.width * 0.85 / 2,
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                            <XYPlot
+                                xType="ordinal"
+                                width={300}
+                                height={300}
+                                xDistance={100}
+                            >
+                                <VerticalGridLines />
+                                <HorizontalGridLines />
+                                <XAxis style={{
+                                    line: { stroke: 'grey' },
+                                }} title="日期" position='start' />
+                                <YAxis style={{
+                                    line: { stroke: 'grey' },
+                                }} title="工作量" />
+                                <BarSeries
+                                    color='rgb(92,172,238)'
+                                    className="vertical-bar-series-example"
+                                    data={[
+                                        { x: 10, y: 5 },
+                                        { x: 11, y: 7 },
+                                        { x: 12, y: 6 },
+                                        { x: 13, y: 4 },
+                                        { x: 14, y: 3 },
+                                        { x: 15, y: 8 },
+                                        { x: 16, y: 5 },
+                                    ]} />
+                            </XYPlot>
+                            <h>{'近期每日发布任务数'}</h>
+                        </div>
+                        <div style={{
+                            display: 'flex',
+                            width: this.state.width * 0.85 / 2,
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                            <RadialChart
+                                colorType={'literal'}
+                                colorDomain={[0, 100]}
+                                colorRange={[0, 10]}
+                                margin={{ top: 100 }}
+                                getLabel={d => d.name}
                                 data={[
-                                    { x: 10, y: 5 },
-                                    { x: 11, y: 7 },
-                                    { x: 12, y: 6 },
-                                    { x: 13, y: 4 },
-                                    { x: 14, y: 3 },
-                                    { x: 15, y: 8 },
-                                    { x: 16, y: 5 },
-                                ]} />
-                        </XYPlot>
-                        <h>{'近期每日发布任务数'}</h>
+                                    { angle: 1, color: '#89DAC1', name: '项目A', opacity: 0.2 },
+                                    { angle: 6, color: '#F6D18A', name: '项目B' },
+                                    { angle: 3, color: '#1E96BE', name: '项目C' },
+                                ]}
+                                labelsRadiusMultiplier={1.1}
+                                labelsStyle={{ fontSize: 16, fill: '#222' }}
+                                showLabels
+                                style={{ stroke: '#fff', strokeWidth: 2 }}
+                                width={400}
+                                height={300} />
+                            <h>{'近期参与项目'}</h>
+                        </div>
                     </div>
-                    <div style={{
-                        display: 'flex',
-                        width: this.state.width * 0.85 / 2,
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}>
-                        <RadialChart
-                            colorType={'literal'}
-                            colorDomain={[0, 100]}
-                            colorRange={[0, 10]}
-                            margin={{ top: 100 }}
-                            getLabel={d => d.name}
-                            data={[
-                                { angle: 1, color: '#89DAC1', name: '项目A', opacity: 0.2 },
-                                { angle: 6, color: '#F6D18A', name: '项目B' },
-                                { angle: 3, color: '#1E96BE', name: '项目C' },
-                            ]}
-                            labelsRadiusMultiplier={1.1}
-                            labelsStyle={{ fontSize: 16, fill: '#222' }}
-                            showLabels
-                            style={{ stroke: '#fff', strokeWidth: 2 }}
-                            width={400}
-                            height={300} />
-                        <h>{'近期参与项目'}</h>
-                    </div>
-                </div>
+                </Scrollbars>
             </div>
         );
     }
